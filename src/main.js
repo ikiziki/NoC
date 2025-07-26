d27ab7a4a81b5a2eb553183c9c4526b7b57e14c9
Darth Vader <Chris.Geese@iCloud.com>
2025-07-26 12:10:17 -0400

    working starfield

--- src/main.js
+++ src/main.js
@@ -1,22 +1,49 @@
-let stars = {}
+let stars = [];
 
 function setup() {
-	createCanvas(windowWidth, windowHeight)
-}
+	createCanvas(windowWidth, windowHeight);
 
-function update() {
+	// create 200 stars
+	for (let i = 0; i < 200; i++) {
+		stars.push(new Star());
+	}
 }
 
 function draw() {
+	background(35); // black background
+
+	for (let star of stars) {
+		star.update();
+		star.draw();
+	}
 }
 
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight);
 }
 
+
 class Star {
 	constructor() {
-		this.x = math.random(0, windowWidth);
-		this.y = math.random(0, windowHeight);
+		this.x = random(0, windowWidth);
+		this.y = random(0, windowHeight)
+		this.speed = random(0.5, 2); // random speed
+		this.size = random(0, 3)
+	}
+
+	update() {
+		this.x += this.speed;
+
+		// if the star goes off the right edge, reset to left
+		if (this.x > windowWidth) {
+			this.x = 0;
+			this.y = random(0, windowHeight);
+			this.speed = random(0.5, 2); // reset speed for variety
+		}
+	}
+
+	draw() {
+		stroke(235);
+		ellipse(this.x, this.y,this.size);
 	}
 }
