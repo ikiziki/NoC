00ef3e20d481c213e27eb5bfbe4a9543e1e673eb
Darth Vader <Chris.Geese@iCloud.com>
2025-07-26 11:13:26 -0400

    random walker

--- src/main.js
+++ src/main.js
@@ -1,13 +1,37 @@
-function setup() {
-	createCanvas(windowWidth, windowHeight)
-}
+let walker;
 
-function update() {
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  walker = new Walker();
 }
 
 function draw() {
+  // background(220); // Clear screen each frame
+  walker.step();
+  walker.draw();
 }
 
 function windowResized() {
-	resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+class Walker {
+  constructor() {
+    this.x = windowWidth / 2;
+    this.y = windowHeight / 2;
+  }
+
+  step() {
+    // Move randomly in X and Y by -1, 0, or 1
+    this.x += random([-1, 0, 1]);
+    this.y += random([-1, 0, 1]);
+
+    // Optional: constrain to stay inside the canvas
+    this.x = constrain(this.x, 0, width);
+    this.y = constrain(this.y, 0, height);
+  }
+
+  draw() {
+    ellipse(this.x, this.y, 10, 10);
+  }
 }
